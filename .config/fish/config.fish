d64ea8f581aeadbca5c0dc0880743f39b0ac6c67
Yvan Satyawan <y_satyawan@hotmail.com>
2019-12-03 11:22:27 +0100

    Added some changes to vimrc and made zhaw.fish way more robust

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,10 +7,18 @@ eval /Users/Yvan/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 
 set -gx LC_ALL en_US.UTF-8
 
-#set fish_greeting
-
+# iterm shell integration
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
+# Color scheme for theme
 set theme_color_scheme terminal2-light-black
 
-alias dotfiles "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+# dotfiles git
+alias dotfiles "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME" 
+
+# Emoji settings
+set fish_emoji_width 2
+set fish_ambiguous_width 1
+
+# Sudo alias
+alias 😠 "sudo"
--- .config/fish/functions/zhaw.fish
+++ .config/fish/functions/zhaw.fish
@@ -1,19 +1,99 @@
 function zhaw
-    if [ -z "$argv" ]
-		echo -e "\033]50;SetProfile=DGX2\a" && ssh saty@dgx2.cloudlab.zhaw.ch && echo -e "\033]50;SetProfile=Default\a"
-	else if [ "$argv[1]" = "1" ]
-        echo -e "\033]50;SetProfile=DGX\a" && ssh saty@dgx.cloudlab.zhaw.ch && echo -e "\033]50;SetProfile=Default\a"
-	else if [ "$argv[1]" = "sh" ]
-		echo "Forwarding port "$argv[2]" from dgx2 server" && ssh -L 127.0.0.1:8888:0.0.0.0:$argv[2] -N saty@dgx2.cloudlab.zhaw.ch
-	else if [ "$argv[1]" = "sh1" ]
-		echo "Forwarding port "$argv[2]" from dgx server" && ssh -L 127.0.0.1:8888:0.0.0.0:$argv[2] -N saty@dgx.cloudlab.zhaw.ch
-	else if [ "$argv[1]" = "tb" ]
-		echo "Forwarding Tensorboard port from dgx2 server" && ssh -L 127.0.0.1:5000:0.0.0.0:9175 -N saty@dgx2.cloudlab.zhaw.ch
-	else if [ "$argv[1]" = "tb1" ]
-		echo "Forwarding Tensorboard port from dgx1 server" && ssh -L 127.0.0.1:5000:0.0.0.0:9175 -N saty@dgx.cloudlab.zhaw.ch
-    else if [ "$argv[1]" = "sync" ]
+    function _zhaw_show_usage
+        echo "usage: zhaw [options]"
+        echo ""
+        echo "Options and arguments:"
+        echo "    -b server          : Selects the corresponding dgx server. E.g. if"
+        echo "                         server=1, selects dgx. If server=2, selects dgx2."
+        echo "                         Defaults to dgx if flag not given."
+        echo "    -f, --forward port : Forwards the given ssh port on the cluster to"
+        echo "                         localhost:8888. Defaults to 9177"
+        echo "    -t, --tensorboard  : Forwards the tensorboard port (9175) on the dgx2 to"
+        echo "                         localhost:5000."
+        echo "    -s, --sync         : Performs an rsync command between the local sync"
+        echo "                         directory with the cluster."
+        echo "    -k, --kill         : Kills any port forwarding sessions. This is a mutually"
+        echo "                         exclusive option from the rest of the options. This is"
+        echo "                         always executed first."
+        echo "    -h, --help         : Shows this help page."
+        functions -e _zhaw_show_usage  # Erase usage
+	end
+
+    # Set the variables
+    set server "dgx"
+    set port "9177"
+    
+    # Parse args and give help is error is thrown
+    argparse --name=zhaw 'b=' 'f/forward=' 't/tensorboard' 's/sync' 'k/kill' 'h/help' -- $argv
+    or _zhaw_show_usage && return 1
+
+    # Show usage is no flags are set or if help flag is set
+    if not begin ; set -q _flag_b ; or set -q _flag_f ; or set -q _flag_t ; or set -q _flag_s ; or set -q _flag_k ; or set -q _flag_h ; end
+        echo Error: zhaw requires at least one option to be selected.
+        _zhaw_show_usage && return 1
+    end
+
+    # Show help if the flag is set and nothing else
+    if set -q _flag_h
+        _zhaw_show_usage && return
+    end
+
+    # Kill flag is mutex with everything else
+    if set -q _flag_k
+        # First get the currently running ssh jobs
+        set running_jobs (jobs | grep -E 'ssh -L 127.0.0.1:(5000|8888):0.0.0.0:\S* -N saty@' | awk '{print $2}')
+        if test -n "$running_jobs"
+            begin 
+                echo Killing $running_jobs
+                and echo $running_jobs | xargs kill
+                and return
+            end
+        else
+            echo "No zhaw jobs are running"
+            return
+        end
+    end
+
+    # Parse server arg
+    if set -q _flag_b
+        if not set -q _flag_f
+            echo "Error: option flag -b does nothing without either option flag -f"
+            return 1
+        end
+        if [ -n "$_flag_b" ]
+            if test $_flag_b -gt 1
+                set server dgx$_flag_b
+            end
+        else
+            set server dgx2
+        end
+    end
+
+    # Parse forwarding arg
+    if set -q _flag_f
+        if [ -n "$_flag_f" ]
+            set port $_flag_f
+        end
+    end
+
+    # Now do cases. First is both forwarding and tensorboard
+    if begin ; set -q _flag_f ; and set -q _flag_t ; end
+        echo 'Forwarding Tensorboard port from '(set_color red)'dgx2'(set_color normal)' and forwarding port '(set_color red)$port(set_color normal)' from '(set_color red)$server(set_color normal)' server'
+        ssh -L 127.0.0.1:8888:0.0.0.0:$port -N saty@$server.cloudlab.zhaw.ch &
+        ssh -L 127.0.0.1:5000:0.0.0.0:9175 -N saty@dgx2.cloudlab.zhaw.ch &
+    
+    # Then if only forwarding
+	else if set -q _flag_f
+		echo "Forwarding port "(set_color red)$port(set_color normal)" from "(set_color red)$server(set_color normal)" server"
+        ssh -L 127.0.0.1:8888:0.0.0.0:$port -N saty@$server.cloudlab.zhaw.ch &
+
+    # And if only tensorboard
+	else if set -q _flag_t
+		echo "Forwarding Tensorboard port from dgx2 server"
+        ssh -L 127.0.0.1:5000:0.0.0.0:9175 -N saty@dgx2.cloudlab.zhaw.ch &
+
+    # The case for sync
+    else if set -q _flag_s
 		rsync -avz --delete /Users/Yvan/OneDrive/ZHAW/Sync/ saty@dgx2.cloudlab.zhaw.ch:/cluster/home/saty/sync
-	else
-		echo "Illegal arguments given"
 	end
 end
